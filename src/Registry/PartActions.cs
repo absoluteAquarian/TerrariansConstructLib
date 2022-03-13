1f83ebb52718e6f88367206f3dea69948918a6e1
absoluteAquarian <jruss88@me.com>
2022-03-13 15:09:46 -0500

    move part actions from library mod

--- src/Registry/PartActions.cs
+++ src/Registry/PartActions.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using Terraria;
-using Terraria.Audio;
-using Terraria.ID;
-using Terraria.ModLoader;
-using TerrariansConstructLib.API;
-using TerrariansConstructLib.ID;
-using TerrariansConstructLib.Items;
+﻿using TerrariansConstructLib.API;
 using TerrariansConstructLib.Materials;
 
 namespace TerrariansConstructLib.Registry {
 	/// <summary>
-	/// A collection of action builders for base Terrarians' Construct materials
+	/// A collection of action builders
 	/// </summary>
 	public static class PartActions {
 		internal static PartsDictionary<ItemPartActionsBuilder> builders;
@@ -19,31 +12,5 @@ namespace TerrariansConstructLib.Registry {
 			=> material is UnloadedMaterial ? new() : builders.Get(material, partID);
 
 		public static readonly ItemPartActionsBuilder NoActions = new();
-
-		public static readonly ItemPartActionsBuilder Copper = new ItemPartActionsBuilder()
-			.WithOnGenericHotkeyUsage((part, player, item) => {
-				var tc = item.ModItem as BaseTCItem;
-
-				if (tc.CopperChargeReady) {
-					tc.copperChargeActivated = true;
-
-					SoundEngine.PlaySound(SoundID.Item92.WithVolume(0.7f).WithPitchVariance(0.1f), player.Center);
-					SoundEngine.PlaySound(SoundID.Item93.WithVolume(0.37f).WithPitchVariance(0.18f), player.Center);
-				}
-			});
-
-		public static readonly ItemPartActionsBuilder Wood = new ItemPartActionsBuilder()
-			.WithModifyWeaponKnockback((int partID, Player player, ref StatModifier knockback, ref float flat) => {
-				knockback -= 0.1f;
-			});
-
-		public static readonly ItemPartActionsBuilder Cobweb = new ItemPartActionsBuilder()
-			.WithItemDefaults((partID, item) => {
-				if (partID != MaterialPartID.WeaponBowString)
-					throw new Exception($"Cobweb is only expected to be used on {nameof(MaterialPartID)}.{nameof(MaterialPartID.WeaponBowString)} parts");
-
-				//No-op, but it's here for an example
-				item.shootSpeed *= 1;
-			});
 	}
 }
