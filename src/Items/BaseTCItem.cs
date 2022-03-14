57c14de7edc3eab776d1b95150a79db343dc105c
absoluteAquarian <jruss88@me.com>
2022-03-14 02:23:14 -0500

    i really need to stop making these small numerous commits

--- src/Items/BaseTCItem.cs
+++ src/Items/BaseTCItem.cs
@@ -30,6 +30,13 @@ namespace TerrariansConstructLib.Items {
 			parts = new(PartsCount);
 		}
 
+		public BaseTCItem(params int[] validPartIDs) {
+			if (validPartIDs.Length != PartsCount)
+				throw new ArgumentException($"Part IDs length ({validPartIDs.Length}) was not equal to the expected length of {PartsCount}");
+
+			parts = new(validPartIDs.Select((p, i) => new ItemPartSlot(i){ isPartIDValid = id => id == p }).ToArray());
+		}
+
 		public BaseTCItem(params ItemPartSlot[] slots) {
 			if (slots.Length != PartsCount)
 				throw new ArgumentException($"Slots length ({slots.Length}) was not equal to the expected length of {PartsCount}");
