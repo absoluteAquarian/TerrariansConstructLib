98efb2cb8f4187ea2d9f07159cca34ca2c0a540d
absoluteAquarian <jruss88@me.com>
2022-03-15 10:20:00 -0500

    improved Extensions.DefaultToMeleeWeapon

--- src/API/Extensions/Extensions.cs
+++ src/API/Extensions/Extensions.cs
@@ -4,9 +4,11 @@ using Terraria.ModLoader;
 
 namespace TerrariansConstructLib.API {
 	public static partial class Extensions {
-		public static void DefaultToMeleeWeapon(this Item item, int singleUseTime, int projOnSwing = ProjectileID.None, float projSpeed = 0f) {
+		public static void DefaultToMeleeWeapon(this Item item, int singleUseTime, int useStyle, bool useTurn = true, int projOnSwing = ProjectileID.None, float projSpeed = 0f) {
 			item.DamageType = DamageClass.Melee;
 			item.useTime = item.useAnimation = singleUseTime;
+			item.useStyle = useStyle;
+			item.useTurn = useTurn;
 
 			if (projOnSwing > ProjectileID.None) {
 				item.shoot = projOnSwing;
