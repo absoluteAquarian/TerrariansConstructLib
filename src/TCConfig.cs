b51ff823f318f3a3334c2b583e0c87c1eead2f45
absoluteAquarian <jruss88@me.com>
2022-03-31 14:34:43 -0500

    added config for making players start with the Fist item instead of the vanilla tools

--- src/TCConfig.cs
+++ src/TCConfig.cs
@@ -12,5 +12,10 @@ namespace TerrariansConstructLib {
 		[Tooltip("Whether constructed items using Terrarians' Construct items should use durability.")]
 		[DefaultValue(true)]
 		public bool UseDurability;
+
+		[Label("Limit Starting Inventory")]
+		[Tooltip("Whether new players start without the Copper tools and with a Fist item instead.")]
+		[DefaultValue(true)]
+		public bool NewPlayersStartWithOnlyHand;
 	}
 }
